2f0e2e8292a66a1ed0e5a6727cafcf86039bb5ec
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2025-01-29 09:38:34 -0600

    3rd person

--- Practice/player_3d.gd
+++ Practice/player_3d.gd
@@ -1,13 +1,21 @@
 extends CharacterBody3D
 
-
-var speed = 5.0
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 7.5
+var speed = WALK_SPEED
 const JUMP_VELOCITY = 4.5
+
+
+var FOV_CHANGE = 1.0
 
 const CAM_SENSITIVITY = 0.03
 @onready var camera = $Head/Camera3D
+@onready var camera_arm = $SpringArm3D
+@onready var camera_pos = camera.position
+@onready var BASE_FOV = camera.fov
 
 var first_person = true
+
 
 const BOB_FREQ = 2.7 
 const BOB_AMP = 0.06
@@ -24,6 +32,10 @@ func _unhandled_input(event: InputEvent) -> void:
 			self.rotate_y(-event.relative.x * (CAM_SENSITIVITY / 15.0))
 			camera.rotate_x(-event.relative.y * (CAM_SENSITIVITY / 15.0))
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		else:
+			self.rotate_y(-event.relative.x * (CAM_SENSITIVITY / 15.0))
+			camera_arm.rotate_x(-event.relative.y * (CAM_SENSITIVITY / 15.0))
+			camera_arm.rotation.x = clamp(camera_arm.rotation.x, deg_to_rad(-75), deg_to_rad(75))
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -31,6 +43,17 @@ func _physics_process(delta: float) -> void:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	if Input.is_action_just_pressed("change_camera"): # V
+		toggle_camera_parent()
+	
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+		FOV_CHANGE = 2.0
+		
+	else:
+		speed = WALK_SPEED
+		FOV_CHANGE = 1.0
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -45,20 +68,35 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		# TODO: Walk/Run Anim
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
 
-	if Input.is_action_just_pressed("sprint"):
-		speed = 10
+	var velocity_clamped = clamp(velocity.length(), 0.5, speed * 2.0)
+	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8.6)
 
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = headbob(t_bob)
 	
 	move_and_slide()
 
+func toggle_camera_parent():
+	var parent = "Head"
+	if first_person:
+		parent = "SpringArm3D"
+		# TODO: Model Visible
+	var child = camera
+	child.get_parent().remove_child(child)
+	get_node(parent).add_child(child)
+	camera = child
+	if not first_person:
+		camera.position = camera_pos
+		# TODO: Model Invis
+	first_person = not first_person
 
 func headbob(time):
 	var pos = Vector3.ZERO
