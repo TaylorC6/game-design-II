ee7e777777c6b0757f873c559da3d88034c43b70
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2025-03-12 09:35:51 -0500

    donecuts

--- Practice/assets/cutscenes/scene2/cutscene_2.gd
+++ Practice/assets/cutscenes/scene2/cutscene_2.gd
@@ -5,7 +5,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animation_playerMain.Stop()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
